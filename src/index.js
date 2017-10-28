3277a30a622388e4c85d44275f91691970b252f2
my0sot1s <manhte231@gmail.com>
2017-10-28 21:01:39 +0700

    client_push_auto

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
-import registerServiceWorker from './helper/registerServiceWorker';
+import registerServiceWorker, { unregister } from './helper/registerServiceWorker';
 import { Provider } from 'react-redux'
 import { BrowserRouter, Route } from 'react-router-dom';
+
+
+
+
 ReactDOM.render(
     <Provider store={require("./stores").default()}>
         <BrowserRouter>
             <Route component={App} />
         </BrowserRouter>
     </Provider>, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
+unregister();
